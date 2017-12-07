7b14d06e06814f5cd7e4296897149a2c5630ee5a
Alexei Karve <karve@us.ibm.com>
2017-12-07 17:08:25 -0500

    switch to ibm provider

--- Customer/Customer.tf
+++ Customer/Customer.tf
@@ -1,10 +1,16 @@
-provider "softlayer" {
-  username = "${var.username}"
-  api_key  = "${var.api_key}"
+#provider "softlayer" {
+#  username = "${var.username}"
+#  api_key  = "${var.api_key}"
+#}
+provider "ibm" {
+}
+
+data "softlayer_ssh_key" "public_key" {
+    label = "ctspkey"
 }
 
 #CTSP Sasg
-resource "softlayer_virtual_guest" "ctsp_sasg1" {
+resource "ibm_compute_vm_instance" "ctsp_sasg1" {
   hostname             = "${var.prefix}ctspSasgOne"
   datacenter           = "${var.datacenter}"
   private_subnet       = "${var.private_subnet}"
@@ -16,5 +22,6 @@ resource "softlayer_virtual_guest" "ctsp_sasg1" {
   memory               = "${var.memory}"
   hourly_billing       = "${var.hourly_billing}"
   local_disk           = "${var.local_disk}"
+  ssh_key_ids          = ["${data.softlayer_ssh_key.public_key.id}"]
 }
 
