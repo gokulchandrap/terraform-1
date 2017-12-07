f13c6dd3ccf8a0cf395608e0c9dd42c376e58065
Alexei Karve <karve@us.ibm.com>
2017-12-07 17:39:06 -0500

    switch to ibm provider

--- ToolsAndFirewall/outputs.tf
+++ ToolsAndFirewall/outputs.tf
@@ -1,24 +1,20 @@
 output "svg_bpm_ipv4_address_private"  {
-  value = "${softlayer_virtual_guest.MSctspBPM.ipv4_address_private}"
+  value = "${ibm_compute_vm_instance.MSctspBPM.ipv4_address_private}"
 }
 
 output "svg_chef_ipv4_address_private"  {
-  value = "${softlayer_virtual_guest.MSctspCHEF.ipv4_address_private}"
-}
-
-output "svg_fw_ip_address_id"  {
-  value = "${softlayer_virtual_guest.MSctspFW.ip_address_id}"
+  value = "${ibm_compute_vm_instance.MSctspCHEF.ipv4_address_private}"
 }
 
 output "svg_fw_ipv6"  {
-  value = "${softlayer_virtual_guest.MSctspFW.ipv6_address}"
+  value = "${ibm_compute_vm_instance.MSctspFW.ipv6_address}"
 }
 
 output "svg_fw_ipv4"  {
-  value = "${softlayer_virtual_guest.MSctspFW.ipv4_address}"
+  value = "${ibm_compute_vm_instance.MSctspFW.ipv4_address}"
 }
 
 output "svg_fw_ipv4_address_private"  {
-  value = "${softlayer_virtual_guest.MSctspFW.ipv4_address_private}"
+  value = "${ibm_compute_vm_instance.MSctspFW.ipv4_address_private}"
 }
 
