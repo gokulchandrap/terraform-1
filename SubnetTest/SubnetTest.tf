86e9728b9dee2889ba833a3b521e52b3143b4c43
Alexei Karve <karve@us.ibm.com>
2017-12-11 14:50:15 -0500

    Subnet test

--- SubnetTest/SubnetTest.tf
+++ SubnetTest/SubnetTest.tf
@@ -6,11 +6,14 @@ resource "ibm_subnet" "test" {
   private = true
   ip_version = 4
   capacity = 8
-  vlan_id = 1545437
+  vlan_id = 2105029
 }
 
 # Use a built-in function cidrhost with index 0.
 output "first_ip_address" {
   value = "${cidrhost(ibm_subnet.test.subnet,0)}"
+}
+output "subnet_id" {
+  value = "${ibm_subnet.test.id}"
 }
 
